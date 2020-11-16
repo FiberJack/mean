e3c0401d9691c2f3786a572a9b2b2f476bedaab5
Dimanskr <46868103+dimanskr@users.noreply.github.com>
2020-11-16 21:30:10 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb+srv://dimanskr:28jan1984@cluster0.wpift.mongodb.net/test?retryWrites=true&w=majority',
+    db: 'mongodb+srv://dimanskr:<PASSWORD>@cluster0.wpift.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'some_itproger'
 }
